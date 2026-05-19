a8e9d211d81c7ee3470720a20386ca6d98d8c15c
rin <chaerin0916@gmail.com>
2026-05-19 21:38:19 +0900

    test(Theme): 테마 테스트 코드 보완

--- src/test/java/roomescape/domain/theme/ThemeTest.java
+++ src/test/java/roomescape/domain/theme/ThemeTest.java
@@ -1,32 +1,104 @@
 package roomescape.domain.theme;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import roomescape.support.exception.RoomescapeException;
 
 class ThemeTest {
 
-    @Test
-    @DisplayName("테마를 생성한다.")
-    void createTheme() {
-        Theme theme = Theme.createWithoutId("테마", "설명", "url");
+    @DisplayName("성공 케이스")
+    @Nested
+    class Success {
 
-        assertThat(theme.getName()).isEqualTo("테마");
-        assertThat(theme.getContent()).isEqualTo("설명");
-        assertThat(theme.getUrl()).isEqualTo("url");
+        @Test
+        @DisplayName("테마를 생성할 수 있다.")
+        void create_theme() {
+            // given
+            Long id = 1L;
+            String name = "테스트 테마";
+            String content = "테마 내용";
+            String url = "/themes/test-theme";
+
+            // when & then
+            assertThatCode(() -> {
+                Theme theme = Theme.of(id, name, content, url);
+            })
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("아이디 없이 테마를 생성할 수 있다.")
+        void create_theme_without_id() {
+            // given
+            String name = "테스트 테마";
+            String content = "테마 내용";
+            String url = "/themes/test-theme";
+
+            // when & then
+            assertThatCode(() -> {
+                Theme theme = Theme.createWithoutId(name, content, url);
+            })
+                .doesNotThrowAnyException();
+        }
     }
 
-    @Test
-    @DisplayName("필수 정보가 누락되면 예외가 발생한다.")
-    void createWithInvalidInfo() {
-        assertThatThrownBy(() -> Theme.createWithoutId(null, "설명", "url"))
-            .isInstanceOf(RoomescapeException.class);
-        assertThatThrownBy(() -> Theme.createWithoutId("테마", null, "url"))
-            .isInstanceOf(RoomescapeException.class);
-        assertThatThrownBy(() -> Theme.createWithoutId("테마", "설명", null))
-            .isInstanceOf(RoomescapeException.class);
+    @DisplayName("실패 케이스")
+    @Nested
+    class Fail {
+
+        @Test
+        @DisplayName("이름이 null이면 예외가 발생한다.")
+        void exception_when_name_null() {
+            // given
+            Long id = 1L;
+            String name = null;
+            String content = "테마 내용";
+            String url = "/themes/test-theme";
+
+            // when & then
+            assertThatThrownBy(() -> {
+                Theme theme = Theme.of(id, name, content, url);
+            })
+                .isInstanceOf(RoomescapeException.class)
+                .hasMessageContaining("테마 명칭 데이터가 유효하지 않습니다.");
+        }
+
+        @Test
+        @DisplayName("내용이 null이면 예외가 발생한다.")
+        void exception_when_content_null() {
+            // given
+            Long id = 1L;
+            String name = "테마";
+            String content = null;
+            String url = "/themes/test-theme";
+
+            // when & then
+            assertThatThrownBy(() -> {
+                Theme theme = Theme.of(id, name, content, url);
+            })
+                .isInstanceOf(RoomescapeException.class)
+                .hasMessageContaining("테마 설명 데이터가 유효하지 않습니다.");
+        }
+
+        @Test
+        @DisplayName("url이 null이면 예외가 발생한다.")
+        void exception_when_url_null() {
+            // given
+            Long id = 1L;
+            String name = "테마";
+            String content = "테마 내용";
+            String url = null;
+
+            // when & then
+            assertThatThrownBy(() -> {
+                Theme theme = Theme.of(id, name, content, url);
+            })
+                .isInstanceOf(RoomescapeException.class)
+                .hasMessageContaining("테마 포스터 URL 데이터가 유효하지 않거나 형식이 올바르지 않습니다.");
+        }
     }
 }
