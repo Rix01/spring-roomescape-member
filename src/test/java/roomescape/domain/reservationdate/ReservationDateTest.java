96d520096b2a4b6a2deb370c893bd964488147fb
rin <chaerin0916@gmail.com>
2026-05-20 11:20:25 +0900

    test(ReservationDate): ReservationDate 테스트 코드 보완

--- src/test/java/roomescape/domain/reservationdate/ReservationDateTest.java
+++ src/test/java/roomescape/domain/reservationdate/ReservationDateTest.java
@@ -2,40 +2,94 @@ package roomescape.domain.reservationdate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import roomescape.support.exception.RoomescapeException;
 
 class ReservationDateTest {
 
-    @Test
-    @DisplayName("예약 날짜를 생성한다.")
-    void createReservationDate() {
-        LocalDate playDay = LocalDate.now();
-        ReservationDate reservationDate = ReservationDate.createWithoutId(playDay);
+    @DisplayName("성공 케이스")
+    @Nested
+    class Success {
 
-        assertThat(reservationDate.getPlayDay()).isEqualTo(playDay);
+        @Test
+        @DisplayName("예약일을 생성할 수 있다.")
+        void create_reservation_date() {
+            // given
+            Long id = 1L;
+            LocalDate playDay = LocalDate.of(2026, 5, 19);
+
+            // when & then
+            assertThatCode(() -> {
+                ReservationDate.of(id, playDay);
+            }).doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("아이디 없이 예약일을 생성할 수 있다.")
+        void create_reservation_date_without_id() {
+            // given
+            LocalDate playDay = LocalDate.of(2026, 5, 19);
+
+            // when & then
+            assertThatCode(() -> {
+                ReservationDate.createWithoutId(playDay);
+            }).doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("오늘 날짜를 등록할 수 있다")
+        void create_reservation_date_today() {
+            // given
+            Long id = 1L;
+            LocalDate today = LocalDate.now();
+            LocalDate playDay = LocalDate.now();
+            ReservationDate reservationDate = ReservationDate.of(id, playDay);
+
+            // when
+            boolean isAvailable = reservationDate.isAvailable(today);
+
+            // then
+            assertThat(isAvailable).isTrue();
+        }
+
+        @Test
+        @DisplayName("과거 날짜는 등록할 수 없다")
+        void cannot_create_play_day_past() {
+            // given
+            Long id = 1L;
+            LocalDate today = LocalDate.now();
+            LocalDate playDay = LocalDate.now().minusDays(1);
+            ReservationDate reservationDate = ReservationDate.of(id, playDay);
+
+            // when
+            boolean isAvailable = reservationDate.isAvailable(today);
+
+            // then
+            assertThat(isAvailable).isFalse();
+        }
     }
 
-    @Test
-    @DisplayName("날짜가 null이면 예외가 발생한다.")
-    void createWithNullDate() {
-        assertThatThrownBy(() -> ReservationDate.createWithoutId(null))
-            .isInstanceOf(RoomescapeException.class);
-    }
+    @DisplayName("실패 케이스")
+    @Nested
+    class Fail {
 
-    @Test
-    @DisplayName("오늘 또는 미래의 날짜인지 확인한다.")
-    void isAvailable() {
-        LocalDate today = LocalDate.now();
-        ReservationDate pastDate = ReservationDate.createWithoutId(today.minusDays(1));
-        ReservationDate todayDate = ReservationDate.createWithoutId(today);
-        ReservationDate futureDate = ReservationDate.createWithoutId(today.plusDays(1));
+        @Test
+        @DisplayName("예약일이 null이면 예외가 발생한다.")
+        void exception_when_play_day_null() {
+            // given
+            Long id = 1L;
+            LocalDate playDay = null;
 
-        assertThat(pastDate.isAvailable(today)).isFalse();
-        assertThat(todayDate.isAvailable(today)).isTrue();
-        assertThat(futureDate.isAvailable(today)).isTrue();
+            // when & then
+            assertThatThrownBy(() -> {
+                ReservationDate.of(id, playDay);
+            }).isInstanceOf(RoomescapeException.class)
+                .hasMessageContaining("예약 날짜 데이터가 유효하지 않거나 누락되었습니다.");
+        }
     }
 }
